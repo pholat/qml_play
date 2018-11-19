221feb70b799c5cd219c9a5c8696c31ea0590da8
pholat <pholat@gmail.com>
2018-11-19 23:21:00 +0100

    added json syntax

--- main.py
+++ main.py
@@ -4,85 +4,58 @@ import sys
 
 from PyQt5 import QtGui, QtCore
 from PyQt5.QtCore import QObject
-from PyQt5.QtGui import QGuiApplication, QSyntaxHighlighter, QTextDocument
+from PyQt5.QtGui import QGuiApplication, QSyntaxHighlighter, QTextDocument, QColor
 from PyQt5.QtWidgets import QTextEdit
 from PyQt5.QtQml import QQmlApplicationEngine, qmlRegisterType
+
+
+def tch_format(fore, bg=None, weight=None):
+    ret = QtGui.QTextCharFormat()
+    ret.setForeground(fore)
+    if weight is not None:
+        ret.setFontWeight(weight)
+    if bg is not None:
+        ret.setBackground(bg)
+    return ret
+
+
+js_patterns = {
+    "string": {
+        "reg": '"((\\\\")|[^"\\\\])*"',  # anything in "" including \"
+        "fmt": tch_format(QtCore.Qt.blue)
+    },
+    "key": {
+        "reg": '"((\\\\")|[^"\\\\])*"\s*:',
+        "fmt": tch_format(QtCore.Qt.blue, weight=QtGui.QFont.Bold)
+    },
+    "num": {
+        "reg": "\d+",
+        "fmt": tch_format(QtCore.Qt.darkBlue)
+    },
+    "type": {
+        "reg": "(true)|(false)|(null)",
+        "fmt": tch_format(QtCore.Qt.red)
+    },
+    "container": {
+        "reg": "[\[\]\{\}]",
+        "fmt": tch_format(QtCore.Qt.darkCyan)
+    }
+}
 
 
 class Highlighter(QSyntaxHighlighter):
     def __init__(self, parent=None):
         super(Highlighter, self).__init__(parent)
 
-        keywordFormat = QtGui.QTextCharFormat()
-        keywordFormat.setForeground(QtCore.Qt.darkBlue)
-        keywordFormat.setFontWeight(QtGui.QFont.Bold)
-
-        keywordPatterns = ["\\bchar\\b", "\\bclass\\b", "\\bconst\\b",
-                "\\bdouble\\b", "\\benum\\b", "\\bexplicit\\b", "\\bfriend\\b",
-                "\\binline\\b", "\\bint\\b", "\\blong\\b", "\\bnamespace\\b",
-                "\\boperator\\b", "\\bprivate\\b", "\\bprotected\\b",
-                "\\bpublic\\b", "\\bshort\\b", "\\bsignals\\b", "\\bsigned\\b",
-                "\\bslots\\b", "\\bstatic\\b", "\\bstruct\\b",
-                "\\btemplate\\b", "\\btypedef\\b", "\\btypename\\b",
-                "\\bunion\\b", "\\bunsigned\\b", "\\bvirtual\\b", "\\bvoid\\b",
-                "\\bvolatile\\b"]
-
-        self.highlightingRules = [(QtCore.QRegExp(pattern), keywordFormat)
-                for pattern in keywordPatterns]
-
-        classFormat = QtGui.QTextCharFormat()
-        classFormat.setFontWeight(QtGui.QFont.Bold)
-        classFormat.setForeground(QtCore.Qt.darkMagenta)
-        self.highlightingRules.append((QtCore.QRegExp("\\bQ[A-Za-z]+\\b"),
-                classFormat))
-
-        singleLineCommentFormat = QtGui.QTextCharFormat()
-        singleLineCommentFormat.setForeground(QtCore.Qt.red)
-        self.highlightingRules.append((QtCore.QRegExp("//[^\n]*"),
-                singleLineCommentFormat))
-
-        self.multiLineCommentFormat = QtGui.QTextCharFormat()
-        self.multiLineCommentFormat.setForeground(QtCore.Qt.red)
-
-        quotationFormat = QtGui.QTextCharFormat()
-        quotationFormat.setForeground(QtCore.Qt.darkGreen)
-        self.highlightingRules.append((QtCore.QRegExp("\".*\""), quotationFormat))
-
-        functionFormat = QtGui.QTextCharFormat()
-        functionFormat.setFontItalic(True)
-        functionFormat.setForeground(QtCore.Qt.blue)
-        self.highlightingRules.append((QtCore.QRegExp("\\b[A-Za-z0-9_]+(?=\\()"), functionFormat))
-
-        self.commentStartExpression = QtCore.QRegExp("/\\*")
-        self.commentEndExpression = QtCore.QRegExp("\\*/")
-
     def highlightBlock(self, text):
-        for pattern, format in self.highlightingRules:
-            expression = QtCore.QRegExp(pattern)
+        self.setFormat(0, len(text), tch_format(QtCore.Qt.red, weight=QtGui.QFont.Bold))
+        for key in js_patterns:
+            expression = QtCore.QRegExp(js_patterns[key]["reg"])
             index = expression.indexIn(text)
             while index >= 0:
                 length = expression.matchedLength()
-                self.setFormat(index, length, format)
+                self.setFormat(index, length, js_patterns[key]["fmt"])
                 index = expression.indexIn(text, index + length)
-
-        self.setCurrentBlockState(0)
-
-        startIndex = 0
-        if self.previousBlockState() != 1:
-            startIndex = self.commentStartExpression.indexIn(text)
-
-        while startIndex >= 0:
-            endIndex = self.commentEndExpression.indexIn(text, startIndex)
-
-            if endIndex == -1:
-                self.setCurrentBlockState(1)
-                commentLength = len(text) - startIndex
-            else:
-                commentLength = endIndex - startIndex + self.commentEndExpression.matchedLength()
-
-            self.setFormat(startIndex, commentLength, self.multiLineCommentFormat)
-            startIndex = self.commentStartExpression.indexIn(text, startIndex + commentLength)
-
 
 def print_child(el):
     print(" " * 2 * print_child.max + "type: {}".format(type(el)))
@@ -108,8 +81,12 @@ if __name__ in "__main__":
     engine = QQmlApplicationEngine()
     engine.load("main.qml")
     print_child(engine.rootObjects()[0])
-    el = Highlighter(
-                child_selector(engine.rootObjects()[0].findChild(QObject, "text_view").children(), QTextDocument)
-            )
+    json_view = child_selector(engine.rootObjects()[0].findChild(QObject, "text_view").children(), QTextDocument)
+    try:
+        with open("main.json") as fp:
+            json_view.setPlainText(fp.read())
+    except FileNotFoundError as err:
+        print("no file main.json")
+    el = Highlighter(json_view)
     engine.quit.connect(app.quit)
     sys.exit(app.exec_())
--- main.qml
+++ main.qml
@@ -7,27 +7,58 @@ Window {
     height: 480
     title: qsTr("Hello")
 
-    TextEdit {
-        id: text_view
-        objectName: "text_view"
-        x: 0
-        y: 0
+    Rectangle {
+        id: bg_rec
+        color: "oldlace"
+        height: parent.height
+        width: parent.width
+    }
+    Rectangle {
+        id: text_view_rect
+        border.width: 4
+        border.color: black
         width: parent.width
         height: parent.height * 0.8
-        color: "black"
-        text: qsTr("long class")
-        font.pixelSize: 12
-        anchors.margins: 5
+        color: bg_rec.color
+
+        Image {
+            source: "chibi_bg.jpg"
+            anchors.bottom: text_view.bottom
+            anchors.horizontalCenter: parent.horizontalCenter
+        }
+
+        TextEdit {
+            id: text_view
+            objectName: "text_view"
+            width: parent.width - 4
+            height: parent.height - 4
+            color: "black"
+            text: qsTr("long class")
+            font.pixelSize: 18
+            anchors.margins: 5
+            anchors.verticalCenter: parent.verticalCenter
+            anchors.horizontalCenter: parent.horizontalCenter
+        }
     }
 
-    TextEdit {
-        id: text_input
-        anchors.top: text_view.bottom
+    Rectangle {
+        anchors.top: text_view_rect.bottom
+        border.width: 4
+        color: bg_rec.color
+        border.color: black
         width: parent.width
-        height: parent.height - text_view.height
-        color: "#ef1d1d"
-        text: qsTr("")
-        font.capitalization: Font.MixedCase
-        font.pixelSize: 12
+        height: parent.height - text_view_rect.height
+
+        TextEdit {
+            id: text_input
+            width: parent.width - parent.border.width*2
+            height: parent.height - parent.border.width*2
+            color: "#ef1d1d"
+            text: qsTr("")
+            font.capitalization: Font.MixedCase
+            font.pixelSize: 18
+            anchors.verticalCenter: parent.verticalCenter
+            anchors.horizontalCenter: parent.horizontalCenter
+        }
     }
 }
